31813f7b4a680e18acc0a9fb5764f74065ddfbe4
Kahlil Batieste <kjbatieste@aggies.ncat.edu>
2026-03-18 22:47:32 -0400

    Created record dictionaries, appended to data list, and returned final dataset

--- analyzer.py
+++ analyzer.py
@@ -6,22 +6,66 @@
 # constant storing the dataset filename
 DATASET = "movies.csv"
 
-# -------------------------------
+# ---------------------------------
 # Step 2: Define load_data function
-# -------------------------------
+# ---------------------------------
 def load_data(filepath):
     """Loads data from a CSV file and returns a list of dictionaries."""
     
     data = []
 
 
-# -------------------------------
-# Step 3: Open the file
-# -------------------------------
+# ---------------------------------------
+# Step 3: Open the file using "with" loop
+# ---------------------------------------
     with open(filepath, "r") as file:
     
     
-        # -------------------------------
-        # Step 4: Read lines from the file
-        # -------------------------------
+        # ---------------------------------------------------------
+        # Step 4: Read lines from the file and store them in a list
+        # ---------------------------------------------------------
         lines = file.readlines()
+
+
+        # -----------------------------------------------------------------
+        # Step 5: Skip the header row only produce lines below the header.
+        # -----------------------------------------------------------------
+        lines = lines[1:]
+
+
+        # -----------------------------------------------------------------------
+        # Step 6: Loop through each line and process into correct data structure.
+        # -----------------------------------------------------------------------
+        for line in lines:  
+
+
+        # -----------------------------------------------------------
+        # Step 7: Split line into parts and store in a formated list.
+        # -----------------------------------------------------------
+            parts = line.strip().split(",") 
+
+        
+        # -------------------------------------------------------------
+        # Step 8: Assign values to variables and store in a dictionary.
+        # -------------------------------------------------------------
+        title = parts[0]
+        year = parts[1]
+        genre = parts[2]
+        rating = parts[3]
+        value = parts[4]
+
+
+    # ---------------------------------------------
+    # Step 9: Create dictionary for 
+    # row and append to data list then return data.
+    # ---------------------------------------------
+    record = {
+        "title": title,
+        "year": year,
+        "genre": genre,
+        "rating": rating,
+        "value": value}
+    data.append(record)
+    return data
+
+
